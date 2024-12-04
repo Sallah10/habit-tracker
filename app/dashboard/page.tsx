1721afcf11ac184b7c451c7206eb7ee9a264b2eb
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2024-12-04 17:13:55 +0100

    Dashboard created and added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,6 +6,7 @@ const dashboard = () => {
     <>
       <section className='bg-[#26252F] min-h-screen pt-10 px-6 gap-16 flex flex-col'>
         <Hero/>
+        dashboard
       </section>
     </>
   )
