01a367cbc2ec56a26d8b763c9b24b879bea3751e
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2024-12-04 21:54:53 +0100

    chsrt component created and added to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Hero from "../hero"
+import { Component } from "../chart"
 
 const dashboard = () => {
   return (
     <>
       <section className='bg-[#26252F] min-h-screen pt-10 px-6 gap-16 flex flex-col'>
         <Hero/>
-        dashboard
+        <div className='flex flex-col gap-6 justify-between items-center'>
+          <div className=' flex border-8 rounded-full items-center justify-center h-[300px] w-[300px] text-lg text-white'>
+            <h1>10h 23m</h1>
+          </div>
+          <div className='text-white flex flex-col justify-center items-center gap-4'>
+            <a href=""><h1 className='text-2xl'> Wind Down</h1><small className='items-center justify-center flex'>off</small></a>
+            <a href=""><h1 className='text-2xl'> Focus Mode</h1> <small className='items-center justify-center flex'>off</small></a>
+            <a href=""><h1 className='text-2xl'> Most Used Apps</h1></a>
+          </div>
+        </div>
+        <Component/>
       </section>
     </>
   )
