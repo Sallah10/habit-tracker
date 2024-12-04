cf42ca2dd0306513adbcc6deb5a36c1835f00499
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2024-12-04 15:45:33 +0100

    home and login page created

---
+++ app/login/page.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Image from 'next/image'
+import logo from "/app/assets/HabiTapp.png"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  // CardDescription,
+  CardFooter,
+  // CardHeader,
+  // CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+// import { Label } from "@/components/ui/label"
+
+const login = () => {
+  return (
+    <>
+      <section className='bg-[#26252F] min-h-screen pt-10 px-6 gap-16 flex flex-col'>
+        <div>
+          <Image src={logo} alt="Logo" className=""/>
+          <h2 className="text-white text-lg ">&quot;Your Habits in an App with just one Tap&quot;</h2>
+        </div>
+        <Card className="bg-transparent border-0">
+          <CardContent>
+            <form>
+              <div className="grid w-full items-center gap-14">
+                <div className="flex flex-col space-y-1.5 gap-4">
+                  <Input id="name" placeholder="User" className='bg-[#D9D9D9] text-center'/>
+                  <Input id="name" placeholder="Password" className='bg-[#D9D9D9] text-center'/>
+                </div>
+                <Button>Login</Button>
+              </div>
+            </form>
+          </CardContent>
+          {/* <Button>Deploy</Button> */}
+          <CardFooter className='text-white'>
+            Don&apos;t have an account? <a href="" className='ml-1'>Sign Up Here</a>
+          </CardFooter>
+        </Card>
+      </section>
+    </>
+  )
+}
+
+export default login
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
-// import Image from "next/image";
+import Image from "next/image";
+import logo from "/app/assets/HabiTapp.png"
+import bgImage from "/app/assets/BG Habit 2.png"
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
     <>
-      <div> app js</div>
+    {/* style={{background-image: ""url('/app/BG Habit 2.png');"}} */}
+    {/* <div 
+      className="bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: `url('C:\Users\DELL\Desktop\habit-tracker\app\BG Habit 2.png')`
+      }}> */}
+      <div className="mx-auto px-6 gap-4 flex flex-col items-center relative min-h-screen w-full">
+        <Image
+          src={bgImage}
+          alt="Background"
+          quality={100}
+          priority
+          className="absolute z-[-1] object-cover w-full h-full"
+        />
+        <div>
+          <Image src={logo} alt="Logo" className="mt-8"/>
+          <h2 className="text-white text-lg ">&quot;Your Habits in an App with just one Tap&quot;</h2>
+        </div>
+        <div  className="mt-64 mx-auto flex self-center">
+          <a href="/login"><Button> Get Started</Button></a>
+        </div>
+      </div>
     </>
   );
 }
