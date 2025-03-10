0e2baf100d7ed16d7ca1b8a4da1db8e06a257f03
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-03-10 12:59:16 +0100

    Remove unused image assets and update import paths in the focus page

--- app/dashboard/focus/page.tsx
+++ app/dashboard/focus/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 import Hero from "../../hero"
 import { Switch } from "@/components/ui/switch"
 import Image from 'next/image'
-import icon5 from "/app/assets/Container Icon-5.png"
-import icon6 from "/app/assets/Container Icon-6.png"
-import icon7 from "/app/assets/Container Icon-7.png"
+import icon5 from "/app/assets/ContainerIcon-5.png"
+import icon6 from "/app/assets/ContainerIcon-6.png"
+import icon7 from "/app/assets/ContainerIcon-7.png"
 
 
 const Focus = () => {
@@ -17,34 +17,34 @@ const Focus = () => {
   return (
     <>
       <section className='general'>
-        <Hero/>
+        <Hero />
         <div className='bg-[#D9D9D9] rounded-lg gap-10 flex flex-col px-4 py-6 mb-10 self-center'>
           <div className='flex justify-between items-center'>
             <h1 className='text-2xl'> Focus Mode </h1>
             <Switch
-            checked={focusMode}
-            onCheckedChange={toggleFocusMode}/>
+              checked={focusMode}
+              onCheckedChange={toggleFocusMode} />
           </div>
-            <div className='flex '>
-              { focusMode ? 
-                 <div className='flex flex-col gap-6'>
-                  <div className='grid grid-cols-2 gap-3 md:grid-cols-2 lg:grid-cols-3 border-b-4 border-gray-600 pb-6'>
-                    <Image src={icon5} alt='focusIcons'/>
-                    <Image src={icon6} alt='focusIcons'/>
-                    <Image src={icon7} alt='focusIcons'/>
-                    <Image src={icon5} alt='focusIcons'/>
-                    <Image src={icon6} alt='focusIcons'/>
-                    <Image src={icon7} alt='focusIcons'/>
-                    <Image src={icon5} alt='focusIcons'/>
-                    <Image src={icon6} alt='focusIcons'/>
-                    <Image src={icon7} alt='focusIcons'/>
-                    <Image src={icon5} alt='focusIcons' className='flex md:hidden lg:hidden'/>
-                  </div>
-                  <h1 className='text-xl text-center flex justify-center items-center'> Pause Distracting apps when you need time to focus</h1>
-                 </div>
-                : 
-               <h1 className='text-xl text-center flex justify-center items-center my-24'> Pause Distracting apps when you need time to focus</h1>}
-            </div>
+          <div className='flex '>
+            {focusMode ?
+              <div className='flex flex-col gap-6'>
+                <div className='grid grid-cols-2 gap-3 md:grid-cols-2 lg:grid-cols-3 border-b-4 border-gray-600 pb-6'>
+                  <Image src={icon5} alt='focusIcons' />
+                  <Image src={icon6} alt='focusIcons' />
+                  <Image src={icon7} alt='focusIcons' />
+                  <Image src={icon5} alt='focusIcons' />
+                  <Image src={icon6} alt='focusIcons' />
+                  <Image src={icon7} alt='focusIcons' />
+                  <Image src={icon5} alt='focusIcons' />
+                  <Image src={icon6} alt='focusIcons' />
+                  <Image src={icon7} alt='focusIcons' />
+                  <Image src={icon5} alt='focusIcons' className='flex md:hidden lg:hidden' />
+                </div>
+                <h1 className='text-xl text-center flex justify-center items-center'> Pause Distracting apps when you need time to focus</h1>
+              </div>
+              :
+              <h1 className='text-xl text-center flex justify-center items-center my-24'> Pause Distracting apps when you need time to focus</h1>}
+          </div>
         </div>
       </section>
     </>
--- app/dashboard/mostusedapp/page.tsx
+++ app/dashboard/mostusedapp/page.tsx
@@ -1,51 +1,51 @@
 import React from 'react'
 import Hero from "../../hero"
 import Image from 'next/image'
-import icon1 from "/app/assets/Container Icon.png"
-import icon2 from "/app/assets/Container Icon-1.png"
-import icon3 from "/app/assets/Container Icon-2.png"
-import icon4 from "/app/assets/Container Icon-3.png"
+import icon1 from "/app/assets/ContainerIcon.png"
+import icon2 from "/app/assets/ContainerIcon-1.png"
+import icon3 from "/app/assets/ContainerIcon-2.png"
+import icon4 from "/app/assets/ContainerIcon-3.png"
 
 
 const Used = () => {
   return (
     <>
       <section className='general p-4'>
-        <Hero/>
+        <Hero />
         <div className='bg-[#D9D9D9] rounded-lg flex flex-col gap-4 px-4 py-6 mb-10'>
           <h1 className='text-2xl self-center flex '> Most Used Apps:</h1>
           <div className='gap-4 grid-cols-1 items-center grid lg:grid-cols-3 md:grid-cols-2'>
             <div className='flex flex-col gap-4 pb-3 justify-center sm:border-b-4 md:border-none border-gray-900'>
               <div className='socials'>
-                <Image src={icon1} alt='Discord'/>
+                <Image src={icon1} alt='Discord' />
                 <div>
                   <h1 className='text-xl'> Discord </h1>
                   <p>2 hours and 17 minutes</p>
                 </div>
               </div>
               <div className='socials'>
-                <Image src={icon1} alt='Discord'/>
+                <Image src={icon1} alt='Discord' />
                 <div>
                   <h1 className='text-xl'> Discord </h1>
                   <p>2 hours and 17 minutes</p>
                 </div>
               </div>
               <div className='socials'>
-                <Image src={icon2} alt='Tiktok'/>
+                <Image src={icon2} alt='Tiktok' />
                 <div>
                   <h1 className='text-xl'> Tiktok </h1>
                   <p>2 hours and 17 minutes</p>
                 </div>
               </div>
               <div className='socials'>
-                <Image src={icon3} alt='Twitter'/>
+                <Image src={icon3} alt='Twitter' />
                 <div>
                   <h1 className='text-xl'> Twitter </h1>
                   <p>2 hours and 17 minutes</p>
                 </div>
               </div>
               <div className='socials'>
-                <Image src={icon4} alt='Snapchat'/>
+                <Image src={icon4} alt='Snapchat' />
                 <div>
                   <h1 className='text-xl'> Snapchat </h1>
                   <p>2 hours and 17 minutes</p>
@@ -54,35 +54,35 @@ const Used = () => {
             </div>
             <div className='flex flex-col gap-4 justify-center pb-3 border-b-4  md:border-none border-gray-900'>
               <div className='socials'>
-                <Image src={icon1} alt='Discord'/>
+                <Image src={icon1} alt='Discord' />
                 <div>
                   <h1 className='text-xl'> Discord </h1>
                   <p>2 hours and 17 minutes</p>
                 </div>
               </div>
               <div className='socials'>
-                <Image src={icon1} alt='Discord'/>
+                <Image src={icon1} alt='Discord' />
                 <div>
                   <h1 className='text-xl'> Discord </h1>
                   <p>2 hours and 17 minutes</p>
                 </div>
               </div>
               <div className='socials'>
-                <Image src={icon2} alt='Tiktok'/>
+                <Image src={icon2} alt='Tiktok' />
                 <div>
                   <h1 className='text-xl'> Tiktok </h1>
                   <p>2 hours and 17 minutes</p>
                 </div>
               </div>
               <div className='socials'>
-                <Image src={icon3} alt='Twitter'/>
+                <Image src={icon3} alt='Twitter' />
                 <div>
                   <h1 className='text-xl'> Twitter </h1>
                   <p>2 hours and 17 minutes</p>
                 </div>
               </div>
               <div className='socials'>
-                <Image src={icon4} alt='Snapchat'/>
+                <Image src={icon4} alt='Snapchat' />
                 <div>
                   <h1 className='text-xl'> Snapchat </h1>
                   <p>2 hours and 17 minutes</p>
@@ -91,35 +91,35 @@ const Used = () => {
             </div>
             <div className='flex flex-col gap-4 justify-center pb-3 border-b-4  md:border-none border-gray-900'>
               <div className='socials'>
-                <Image src={icon1} alt='Discord'/>
+                <Image src={icon1} alt='Discord' />
                 <div>
                   <h1 className='text-xl'> Discord </h1>
                   <p>2 hours and 17 minutes</p>
                 </div>
               </div>
               <div className='socials'>
-                <Image src={icon1} alt='Discord'/>
+                <Image src={icon1} alt='Discord' />
                 <div>
                   <h1 className='text-xl'> Discord </h1>
                   <p>2 hours and 17 minutes</p>
                 </div>
               </div>
               <div className='socials'>
-                <Image src={icon2} alt='Tiktok'/>
+                <Image src={icon2} alt='Tiktok' />
                 <div>
                   <h1 className='text-xl'> Tiktok </h1>
                   <p>2 hours and 17 minutes</p>
                 </div>
               </div>
               <div className='socials'>
-                <Image src={icon3} alt='Twitter'/>
+                <Image src={icon3} alt='Twitter' />
                 <div>
                   <h1 className='text-xl'> Twitter </h1>
                   <p>2 hours and 17 minutes</p>
                 </div>
               </div>
               <div className='socials'>
-                <Image src={icon4} alt='Snapchat'/>
+                <Image src={icon4} alt='Snapchat' />
                 <div>
                   <h1 className='text-xl'> Snapchat </h1>
                   <p>2 hours and 17 minutes</p>
