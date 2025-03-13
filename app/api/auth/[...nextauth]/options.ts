ec5aa6f3f866f12ac2c156f9a6c086145151fece
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-03-13 17:42:42 +0100

    Fix type assertion for user ID in session handling and build for production (start app)

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -55,7 +55,7 @@ export const authOptions: NextAuthOptions = {
     },
     async session({ session, token }) {
       if (token?.id) {
-        session.user.id = token.id;
+        session.user.id = token.id as string;
       }
       return session;
     },
