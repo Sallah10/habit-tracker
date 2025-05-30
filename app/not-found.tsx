e8d4c7abdff798fb9f1f7e48e9c8c5c57ab781e9
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-05-30 02:18:37 +0100

    Refactor: Update button styling in NotFound component for better visibility

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
             <h1 className="text2xl">404 - Page Not Found</h1>
             <p className="text-xl">The page you are looking for does not exist.</p>
-            <button onClick={() => router.push("/")} className=" hover:bg-slate-700">Go to Home</button>
+            <button onClick={() => router.push("/")} className=" hover:text-slate-700 underline">Go to Home</button>
         </div>
     );
 }
